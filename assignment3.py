ccae355480040b0f0c64cabe655e5f21520215a0
moonsol124 <moonsol124@gmail.com>
2024-10-18 11:08:50 +0200

    4

--- assignment3.py
+++ assignment3.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from random import randint
+from random import randint, shuffle
 
 # solution version 1: without extensions
 
@@ -13,15 +13,14 @@ def show_duo_names():
     print('│ duo partner 2       │ name 2                │')
     print('└─────────────────────┴───────────────────────┘')
 
-
 class MemoryTestWindow:
-    def __init__(self, root):
+    def __init__(self, root, msInvisible, msBetween, sequenceHiding):
         self.root = root
         self.fontSize = 20
         self.fontFamily = "Helvetica"
         self.fontWeight = "bold"
-        self.screenWidth = 1200
-        self.defaultPadding = 10
+        self.screenWidth = 1700
+        self.defaultPadding = 20
         self.count = 5
         self.frame = None
         self.container1 = None
@@ -40,12 +39,19 @@ class MemoryTestWindow:
         self.block2 = None
         self.block3 = None
         self.block4 = None
+        self.blocks = [self.block1, self.block2, self.block3, self.block4]
         self.blockContainer = None
-        self.blockWidth = 20
-        self.blockHeight = 2
+        self.blockWidth = 300
+        self.blockHeight = 300
+        self.blockPadding = 20
+        self.msInvisible = msInvisible
+        self.msBetween = msBetween
+        self.sequenceHiding = sequenceHiding
+        self.blockColors = ["blue", "red", "green", "yellow"]
+    
 
     def initializeScreen(self):
-        self.frame = Frame(self.root, width=self.screenWidth, height=500, bg="white")
+        self.frame = Frame(self.root, width=self.screenWidth, height=800, bg="white")
         self.frame.pack_propagate(False)  
         self.frame.pack(padx=self.defaultPadding, pady=self.defaultPadding)
 
@@ -53,7 +59,7 @@ class MemoryTestWindow:
         self.container1.pack_propagate(False)  
         self.container1.pack()
 
-        self.container2 = Frame(self.frame, width=self.screenWidth, height=200, bg="gray")
+        self.container2 = Frame(self.frame, width=self.screenWidth, height=600, bg="gray")
         self.container2.pack_propagate(False)
         self.container2.pack(fill="both", expand=True)
 
@@ -81,59 +87,124 @@ class MemoryTestWindow:
         self.container3Label3 = Label(self.container3, text="sequence length", font=(self.fontFamily, self.fontSize, self.fontWeight))  # Third label
         self.container3Label3.grid(row=0, column=5, padx=self.defaultPadding, pady=self.defaultPadding)
 
-        self.container3Input1 = Entry(self.container3)  # First input field
+        input1 = StringVar()
+        input2 = StringVar()
+        input3 = StringVar()
+        input1.set(self.msInvisible) 
+        input2.set(self.msBetween) 
+        input3.set(self.sequenceHiding) 
+
+        self.container3Input1 = Entry(self.container3, textvariable=input1, font=(self.fontFamily, self.fontSize, self.fontWeight))  # First input field
         self.container3Input1.grid(row=0, column=2, padx=self.defaultPadding, pady=self.defaultPadding)
-        self.container3Input2 = Entry(self.container3)  # Second input field
+        self.container3Input2 = Entry(self.container3, textvariable=input2, font=(self.fontFamily, self.fontSize, self.fontWeight))  # Second input field
         self.container3Input2.grid(row=0, column=4, padx=self.defaultPadding, pady=self.defaultPadding)
-        self.container3Input3 = Entry(self.container3)  # Third input field
+        self.container3Input3 = Entry(self.container3, textvariable=input3, font=(self.fontFamily, self.fontSize, self.fontWeight))  # Third input field
         self.container3Input3.grid(row=0, column=6, padx=self.defaultPadding, pady=self.defaultPadding)
     
+    def countDown(self):
+        for second in range(1):
+        # for second in range(self.count):
+            self.root.after(second*1000, self.countToBeginObservationalPhase)
+
     def startCounting(self):
         self.topMessageBox.config(text="Counting down...")
         self.startBtn.config(state="disabled")
-        self.countToBeginObservationalPhase()
-        self.root.after(1000, self.countToBeginObservationalPhase)
-        self.root.after(2000, self.countToBeginObservationalPhase)
-        self.root.after(3000, self.countToBeginObservationalPhase)
-        self.root.after(4000, self.countToBeginObservationalPhase)
+        self.countDown()
 
     def convertToDot(self):
         dots = ""
-        for i in range(self.count):
+        for i in range(self.count-1):
             dots += '.'
         return dots
 
     def beginObservationalPhase(self):
-        self.blockContainer = Frame(self.container2, width=800, height=400, bg="gray")
-        self.blockContainer.pack_propagate(False)  # Prevent container2 from resizing to fit its contents
+        # notes
+        # first, observational phase. you click on number of squares that you have to recall later.
+        # the number of squares to be clicked is specified as sequence length.
+        # after hiding the correct amount of squares, you go to the recall phase.
+        # in recall phase, it's the same. you click on squares. but those squares should match
+        # as the squares that you hid.
+        # then it's done.
+
+        self.blockContainer = Frame(self.container2, width=800, height=700, bg="gray")
         self.blockContainer.place(relx=0.5, rely=0.5, anchor='center')  # Center the frame in the window
+        self.blockContainer.grid_rowconfigure(0, minsize=300)  # Set min height for row 0
+        self.blockContainer.grid_rowconfigure(1, minsize=300)  # Set min height for row 1
+        self.blockContainer.grid_columnconfigure(0, minsize=300)  # Set min width for column 0
+        self.blockContainer.grid_columnconfigure(1, minsize=300)  # Set min width for column 1
 
-        self.block1 = Label(self.blockContainer, font=(self.fontFamily, self.fontSize, self.fontWeight), bg="red", width=self.blockWidth, height=self.blockHeight)  
-        self.block1.grid(row=0, column=0, padx=self.defaultPadding, pady=self.defaultPadding)  
+        self.block1 = Frame(self.blockContainer, bg="blue")  
+        self.block1.grid(row=0, column=0, sticky="nsew", padx=self.defaultPadding, pady=self.defaultPadding)  
 
-        self.block2 = Label(self.blockContainer, font=(self.fontFamily, self.fontSize, self.fontWeight), bg="blue", width=self.blockWidth, height=self.blockHeight) 
-        self.block2.grid(row=0, column=1, padx=self.defaultPadding, pady=self.defaultPadding) 
+        self.block2 = Frame(self.blockContainer, bg="red")   
+        self.block2.grid(row=0, column=1, sticky="nsew", padx=self.defaultPadding, pady=self.defaultPadding) 
 
-        self.block3 = Label(self.blockContainer, font=(self.fontFamily, self.fontSize, self.fontWeight), bg="black", width=self.blockWidth, height=self.blockHeight)  
-        self.block3.grid(row=1, column=0, padx=self.defaultPadding, pady=self.defaultPadding)  
+        self.block3 = Frame(self.blockContainer, bg="green")   
+        self.block3.grid(row=1, column=0, sticky="nsew", padx=self.defaultPadding, pady=self.defaultPadding) 
 
-        self.block4 = Label(self.blockContainer, font=(self.fontFamily, self.fontSize, self.fontWeight), bg="white", width=self.blockWidth, height=self.blockHeight) 
-        self.block4.grid(row=1, column=1, padx=self.defaultPadding, pady=self.defaultPadding) 
+        self.block4 = Frame(self.blockContainer, bg="yellow")   
+        self.block4.grid(row=1, column=1, sticky="nsew", padx=self.defaultPadding, pady=self.defaultPadding) 
+
+        self.block1.grid_propagate(False)
+        self.block2.grid_propagate(False)
+        self.block3.grid_propagate(False)
+        self.block4.grid_propagate(False)
+
+        self.blocks = [self.block1, self.block2, self.block3, self.block4]
+
+        self.topMessageBox.config(text="Watch the sequence...")        
+        self.bindBlocks()
+        # self.randomizeSequence()
+
+    def randomizeSequence(self):
+        shuffle(self.blockColors)
+        for block in range(len(self.blocks)):
+            self.blocks[block].config(bg=self.blockColors[block])
+
+    def hideBlock(self, event):
+        # Access the widget that triggered the event
+        self.unbindBlocks()
+        blockToBeHidden = event.widget
+        blockToBeHidden.grid_remove()
+        blockToBeHidden.after(self.msInvisible, lambda: self.showBlock(blockToBeHidden))
+
+        # for block in range(len(self.blocks)):
+        #     if (blockToBeHidden != self.blocks[block]):
+        #         self.blocks[block].grid()
+
+    def bindBlocks(self):
+        for block in range(len(self.blocks)):
+            self.blocks[block].bind("<Button-1>", self.hideBlock)
+
+    def unbindBlocks(self):
+        for block in range(len(self.blocks)):
+            self.blocks[block].unbind("<Button-1>")
+
+    def showBlock(self, block):
+        # Show the frame again using grid()
+        block.grid()
+        self.root.after(self.msBetween, self.bindBlocks)
+        # there must be a delay between when blocks are all visible and a block is clicable again.
+
 
     def countToBeginObservationalPhase(self):
-        if (self.count == 1):
+        if (self.count == 5):
+            print ("triggered?")
             self.countingMessage.destroy()
             self.beginObservationalPhase()
             return
-
+        
         self.countingMessage.config(text=f"{self.convertToDot()}")
         self.count = self.count - 1
 
 def main():
-    show_duo_names()
+    msInvisible = 1000
+    msBetween = 1000
+    sequenceHiding = 3
 
+    show_duo_names()
     root = Tk()  
-    window = MemoryTestWindow(root)
+    window = MemoryTestWindow(root, msInvisible, msBetween, sequenceHiding)
     root.title("Memory Test")
     window.initializeScreen()
     window.initializeWidgets()
